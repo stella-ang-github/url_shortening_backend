e3a9ae509e558ae195e48651eac857a22c368121
Ang Yi Qi (Stella) <stellaayq@gmail.com>
2022-07-17 18:02:39 +0800

    made changes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
 var cors = require("cors");
-const path = require("path");
 
 const app = express();
 
@@ -10,16 +9,11 @@ connectDB();
 
 app.use(cors());
 app.use(express.json({ extended: false })); //Accept json into our API
-app.use(express.static(path.join(__dirname, "client", "build")));
 
 //Routes
 app.use("/", require("./routes/index"));
 app.use("/api/url", require("./routes/url"));
 
 const PORT = process.env.PORT || 9000;
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
