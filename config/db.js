b5310cbad3436d33ae096fdd4bd989425d030b58
Ang Yi Qi (Stella) <stellaayq@gmail.com>
2022-07-17 17:52:36 +0800

    changed env and mongodb info

--- config/db.js
+++ config/db.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true
-        });
+  try {
+    await mongoose.connect(process.env.MONGODB_URI || db, {
+      useNewUrlParser: true,
+    });
 
-        console.log('MongoDB connected!')
-    } catch (err) {
-        console.log(err.message);
-        process.exit(1); //Exit with failure
-    }
-}
+    console.log("MongoDB connected!");
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1); //Exit with failure
+  }
+};
 
 module.exports = connectDB;
