34226aa3f6598d56752dcadce52a1d6cea88cec4
Ang Yi Qi (Stella) <stellaayq@gmail.com>
2022-07-17 17:28:51 +0800

    Made changes in app.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 var cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -9,11 +10,16 @@ connectDB();
 
 app.use(cors());
 app.use(express.json({ extended: false })); //Accept json into our API
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 //Routes
 app.use("/", require("./routes/index"));
 app.use("/api/url", require("./routes/url"));
 
 const PORT = process.env.PORT || 9000;
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
